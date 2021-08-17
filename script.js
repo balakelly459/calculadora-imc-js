1f77f355907a21902e6a6753a82a96ae6917514d
KELLY MARQUES BALAZSHAZI <21192911@CFP127ALUNO.LOCAL>
2021-08-17 08:51:55 -0300

    almost

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ const resultado = document.getElementById("resultado")
 
 function calcularImc() {
    const imc = parseFloat(peso.value) / (parseFloat(altura.value) ** 2);
-   resultado.textContent = imc
+   resultado.textContent = imc.toFixed(2)
 }
 
+// if (valorImc < 18.5) {
+//    IMC = 'abaixo do peso'
+//    } else if (valorImc >= 18.5 && valorImc < 25) {
+//    IMC = 'com o peso ideal!'
+//    } else if (valorImc >= 25 && valorImc < 30) {
+//    IMC = 'acima do peso.'
+//    } else if (valorImc >= 30 && valorImc < 35) {
+//    IMC = 'com obesidade grau I.'
+//    } else if (valorImc >= 35 && valorImc < 40) {
+//    IMC = 'com obesidade grau II.'
+//    } else {
+//    IMC = 'com obesidade grau III.'
+//    }
+//    resultado.textContent = `${nome}, seu IMC é ${valorImc} e você está
+//    ${IMC}`
+
+
 calcular.addEventListener("click" ,calcularImc)
+
+   
 
